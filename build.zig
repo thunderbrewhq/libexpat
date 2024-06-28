0ba38e5d5ef2928c1a9e1c06b2d22ed80219f2e9
superp00t <superp00t@tutanota.com>
2024-06-28 18:27:19 -0400

    feat(build): ziggify build process

---
+++ build.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+  const target = b.standardTargetOptions(.{});
+  const optimize = b.standardOptimizeOption(.{});
+
+  // const t = target.result;
+
+  // libexpat
+  const expat = b.addStaticLibrary(.{
+    .name = "expat",
+    .target = target,
+    .optimize = optimize
+  });
+  // Link C standard library
+  expat.linkLibC();
+
+  // Include "expat/lib"
+  expat.addIncludePath(b.path("expat/lib"));
+
+  expat.addCSourceFiles(.{
+    .files = &.{
+      "expat/lib/xmlparse.c",
+      "expat/lib/xmlrole.c",
+      "expat/lib/xmltok_impl.c",
+      "expat/lib/xmltok_ns.c",
+      "expat/lib/xmltok.c"
+    }
+  });
+
+  b.installArtifact(expat);
+}
